e195e17d929f8adbaa2b51b312e951083f50ae20
DANTLA NAGI REDDY <78993887+dnr21@users.noreply.github.com>
2022-01-26 19:19:49 +0530

    Create If else no.py

---
+++ If else no.py
@@ -0,0 +1,15 @@
+num = 5
+
+if num > 0:
+
+    print(num, "is a positive number.")
+
+print("This is always printed.")
+
+num = -1
+
+if num > 0:
+
+    print(num, "is a positive number.")
+
+print("This is also always printed.")
