ed11023ba2a7360951b9ec8e1a7302649ac959be
DANTLA NAGI REDDY <78993887+dnr21@users.noreply.github.com>
2022-03-19 22:32:20 +0530

    Create Type conversion.py

---
+++ Type conversion.py
@@ -0,0 +1,7 @@
+num_int = 120
+num_flo = 1.20
+num_new = num_int + num_fl0
+print("datatype of num_int:",type(num_int))
+print("datatype of num_flo:",type(num_flo))
+print("Value of num_new:",num_new)
+print("datatype of num_new:",type(num_new))
