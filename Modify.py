3078923e81ffc3c8e776f55f879c748b43c25924
DANTLA NAGI REDDY <78993887+dnr21@users.noreply.github.com>
2022-05-13 23:01:23 +0530

    Create Modify.py

---
+++ Modify.py
@@ -0,0 +1,9 @@
+import datetime
+
+import pathlib
+
+fname = pathlib.Path('abc.py')
+
+print("Last modification time: %s" % datetime.datetime.fromtimestamp(fname.stat().st_mtime))
+
+print("Last metadata change time or path creation time: %s" % datetime.datetime.fromtimestamp(fname.stat().st_ctime
