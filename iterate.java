274cf055f7c67a8288328a122a87f8b56dd86710
DANTLA NAGI REDDY <78993887+dnr21@users.noreply.github.com>
2022-07-06 22:20:14 +0530

    Create iterate.java

---
+++ iterate.java
@@ -0,0 +1,12 @@
+import java.util.*;  
+public class IterateList
+{  
+public static void main(String args[])   
+{  
+List<String> city = Arrays.asList("Goa", "Delhi", "Mumbai", "Vizag");  
+  for (int i = 0; i < city.size(); i++)   
+{  
+System.out.println(city.get(i));   
+}  
+}  
+}  
